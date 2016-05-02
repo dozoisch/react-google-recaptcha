6721e3f6ed78a67d63acde25380ed3c4dbdff958
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-05-02 01:18:03 -0300

    Added a temporal solution to the lang issue, letting configure the language from an external global variable

--- src/recaptcha-wrapper.js
+++ src/recaptcha-wrapper.js
@@ -2,7 +2,10 @@ import ReCAPTCHA from "./recaptcha";
 import makeAsyncScriptLoader from "react-async-script";
 
 const callbackName = "onloadcallback";
-const URL = `https://www.google.com/recaptcha/api.js?onload=${callbackName}&render=explicit`;
+const lang = (window.recaptchaOptions && window.recaptchaOptions.lang)
+							? "&hl=" + window.recaptchaOptions.lang
+							: "";
+const URL = `https://www.google.com/recaptcha/api.js?onload=${callbackName}&render=explicit${lang}`;
 const globalName = "grecaptcha";
 
 export default makeAsyncScriptLoader(ReCAPTCHA, URL, {
