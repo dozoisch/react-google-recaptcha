42ac75d5d647de228fdd9a55de9b5ed7ced5ddbe
Peter Swallow <peter.swallow@gmail.com>
2019-11-15 10:44:54 +0000

    Made import of React.Component more specific
    Fixes issue #158 when using Preact.js V10+ which now has no `export default` for `Preact` and won't by default include Preact.Component in the import.

--- src/recaptcha.js
+++ src/recaptcha.js
@@ -1,7 +1,7 @@
-import React from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
 
-export default class ReCAPTCHA extends React.Component {
+export default class ReCAPTCHA extends Component {
   constructor() {
     super();
     this.handleExpired = this.handleExpired.bind(this);
